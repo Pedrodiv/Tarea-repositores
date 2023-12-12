afa5cde23a93e4ca23ec96a6cc355be99a096367
Pedrodiv <153134950+Pedrodiv@users.noreply.github.com>
2023-12-11 21:49:27 -0300

    tarea complatada y modificada

---
+++ script.js
@@ -0,0 +1,11 @@
+let num1 = 1;
+let num2 = 4;
+let num3 = 3;
+
+    if(num1>=num2 && num1>=num3){
+        console.log("El número más grande es: " + num1);
+    }else if (num2>=num1 && num2>=num3){
+        console.log("El número más grande es: " + num2);
+    }else if (num3>=num1 && num3>=num2){
+        console.log("El número más grande es: " + num3);
+    }
